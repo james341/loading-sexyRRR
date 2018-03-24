e2c9d043c41d4b151b658536ae7b72488d070003
james341 <taine049@gmail.com>
2018-03-24 16:44:45 +1100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -96,7 +96,7 @@ var l_musicDisplay = true;
 Place how much .ogg songs you want
 (Put the files in the songs folder)*/
 var l_musicPlaylist = [
-	{youtube: "gkvbAOgEKLI", name: "Freedom Networks - Installation Music"},
+	{youtube: "SBdgk2LEQ1s", name: "Freedom Networks - Pumped Up Kicks"},
 	
 ];
 
